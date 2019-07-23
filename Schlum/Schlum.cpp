ed22a764a4df9b70795801e89a62fb8340dbf733
Matija Akrap <matija.akrap@gmail.com>
2019-07-23 11:02:35 +0200

    Dodavanje logike

--- Schlum/Schlum.cpp
+++ Schlum/Schlum.cpp
@@ -3,11 +3,13 @@
 
 #include "stdafx.h"
 #include "Schlum.h"
+#include "String.h"
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
 #include <nana\gui\element.hpp>
 #include <nana/gui/widgets/button.hpp>
 #include <nana/paint/image.hpp>
+#include <nana/gui/timer.hpp>
 
 using namespace nana;
 using namespace paint;
@@ -25,9 +27,96 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+void end() {
+
+	form fm;
+	fm.size(size(711, 400));
+
+	fm.show();
+	exec();
+};
+
 void the_game() {
 	form fm;
 	fm.size(size(711, 400));
+
+	label lb{ fm, rectangle{ 75, 20, 140, 50 } };
+	API::effects_bground(lb, effects::bground_transparent(0), 0);
+	lb.typeface(font{
+							 "Bauhaus 93",
+							 35,
+							 { 700 }
+		});
+	lb.fgcolor(colors::yellow);
+	lb.caption("Time");
+
+	label lb_cd{ fm, rectangle{ 50, 70, 182, 282 } };
+	API::effects_bground(lb_cd, effects::bground_transparent(0), 0);
+	lb_cd.typeface(font{
+							 "Bauhaus 93",
+							 90,
+							 { 700 }
+		});
+	lb_cd.fgcolor(colors::black);
+
+	timer tm{ std::chrono::seconds{1} };
+	tm.elapse([&lb_cd, &tm] {
+		static int i = 2;
+		lb_cd.caption(std::to_string(i--));
+		if (i <= -1) {
+			timer tm2{ std::chrono::seconds{1} };
+			static int j = 2;
+			lb_cd.caption(std::to_string(j--));
+			if (j<= -1) {
+				timer tm3{ std::chrono::seconds{1} };
+				static int k = 2;
+				lb_cd.caption(std::to_string(k--));
+				if (k <= -1) {
+					timer tm3{ std::chrono::seconds{1} };
+					static int l = 2;
+					lb_cd.caption(std::to_string(l--));
+					if (l <= -1 && l!=100) {
+						l = 100;
+						API::exit_all();
+					}
+				}
+			}
+		};
+		});
+	tm.start();
+
+	button btn_schli_schla{ fm, rectangle{260, 70, 182, 282} };
+	element::bground bground2;
+	bground2.image(image("..\\..\\Schlum\\Schlum\\schli_bur.bmp"), true, {});
+	API::effects_bground(btn_schli_schla, effects::bground_transparent(0), 0);
+	bground2.stretch_parts(4, 4, 4, 4);
+	btn_schli_schla.set_bground(bground2);
+
+	label lb2{ fm, rectangle{ 541, 20, 150, 50 } };
+	API::effects_bground(lb2, effects::bground_transparent(0), 0);
+	lb2.typeface(font{
+							 "Bauhaus 93",
+							 35,
+							 { 700 }
+		});
+	lb2.fgcolor(colors::yellow);
+	lb2.caption("Points");
+
+	label lb_pt{ fm, rectangle{479, 70, 182, 282 } };
+	API::effects_bground(lb_pt, effects::bground_transparent(0), 0);
+	lb_pt.typeface(font{
+							 "Bauhaus 93",
+							 90,
+							 { 700 }
+		});
+	lb_pt.fgcolor(colors::black);
+	int pt = 0;
+	lb_pt.caption("0");
+	btn_schli_schla.events().click([&lb_pt, &pt] {
+		++pt;
+		lb_pt.caption(std::to_string(pt));
+		});
+	
 
 	image img("..\\..\\Schlum\\Schlum\\background2.bmp");
 	drawing dw(fm);
@@ -81,6 +170,15 @@ void entry_window() {
 	bground2.stretch_parts(4, 4, 4, 4);
 	btn_schli.set_bground(bground2);
 
+
+	button btn_schla{ fm, rectangle{479, 70, 182, 282} };
+	element::bground bground3;
+	bground3.image(image("..\\..\\Schlum\\Schlum\\schli_bur.bmp"), true, {});
+	API::effects_bground(btn_schla, effects::bground_transparent(0), 0);
+	bground3.stretch_parts(4, 4, 4, 4);
+	btn_schla.set_bground(bground3);
+
+
 	label lb2{ fm, rectangle{ 551, 20, 140, 50 } };
 	API::effects_bground(lb2, effects::bground_transparent(0), 0);
 	lb2.typeface(font{
@@ -108,5 +206,5 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
-	entry_window();
+	the_game();
 }
