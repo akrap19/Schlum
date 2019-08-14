a5841142f6bdd7d76c6358bd7875240c6f5ba8a8
Matija Akrap <matija.akrap@gmail.com>
2019-08-14 10:06:59 +0200

    uređenje drugog prozora

--- Schlum/Resource.h
+++ Schlum/Resource.h
@@ -24,6 +24,11 @@
 #define IDB_BITMAP9                     141
 #define IDB_PNG2                        142
 #define IDB_BITMAP10                    143
+#define IDB_BITMAP11                    144
+#define IDB_BITMAP12                    145
+#define IDB_BITMAP13                    146
+#define IDB_BITMAP14                    147
+#define IDB_BITMAP15                    148
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -31,7 +36,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_RESOURCE_VALUE        149
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
--- Schlum/Schlum.cpp
+++ Schlum/Schlum.cpp
@@ -39,6 +39,7 @@ void end() {
 void the_game() {
 	form fm;
 	fm.size(size(711, 400));
+	fm.caption("Schlum");
 
 	label lb{ fm, rectangle{ 75, 20, 140, 50 } };
 	API::effects_bground(lb, effects::bground_transparent(0), 0);
@@ -58,39 +59,24 @@ void the_game() {
 							 { 700 }
 		});
 	lb_cd.fgcolor(colors::black);
+	
+	
 
-	timer tm{ std::chrono::seconds{1} };
-	tm.elapse([&lb_cd, &tm] {
-		static int i = 2;
-		lb_cd.caption(std::to_string(i--));
-		if (i <= -1) {
-			timer tm2{ std::chrono::seconds{1} };
-			static int j = 2;
-			lb_cd.caption(std::to_string(j--));
-			if (j<= -1) {
-				timer tm3{ std::chrono::seconds{1} };
-				static int k = 2;
-				lb_cd.caption(std::to_string(k--));
-				if (k <= -1) {
-					timer tm3{ std::chrono::seconds{1} };
-					static int l = 2;
-					lb_cd.caption(std::to_string(l--));
-					if (l <= -1 && l!=100) {
-						l = 100;
-						API::exit_all();
-					}
-				}
-			}
-		};
-		});
-	tm.start();
-
-	button btn_schli_schla{ fm, rectangle{260, 70, 182, 282} };
+	/*button btn_schli{ fm, rectangle{260, 70, 182, 282} };
 	element::bground bground2;
 	bground2.image(image("..\\..\\Schlum\\Schlum\\schli_bur.bmp"), true, {});
-	API::effects_bground(btn_schli_schla, effects::bground_transparent(0), 0);
+	API::effects_bground(btn_schli, effects::bground_transparent(0), 0);
 	bground2.stretch_parts(4, 4, 4, 4);
-	btn_schli_schla.set_bground(bground2);
+	btn_schli.set_bground(bground2);*/
+
+
+	button btn_schla{ fm, rectangle{260, 70, 182, 282} };
+	element::bground bground3;
+	bground3.image(image("..\\..\\Schlum\\Schlum\\schli_game.bmp"), false, rectangle{0, 0 ,862,225 });
+	API::effects_bground(btn_schla, effects::bground_transparent(0), 0);
+	bground3.stretch_parts(4, 4, 4, 4);
+	btn_schla.set_bground(bground3);
+
 
 	label lb2{ fm, rectangle{ 541, 20, 150, 50 } };
 	API::effects_bground(lb2, effects::bground_transparent(0), 0);
@@ -102,6 +88,16 @@ void the_game() {
 	lb2.fgcolor(colors::yellow);
 	lb2.caption("Points");
 
+	label lb_state{ fm, rectangle{ 220, 5, 300, 50 } };
+	API::effects_bground(lb_state, effects::bground_transparent(0), 0);
+	lb_state.typeface(font{
+							 "Bauhaus 93",
+							 35,
+							 { 700 }
+		});
+	lb_state.fgcolor(colors::yellow);
+	
+
 	label lb_pt{ fm, rectangle{479, 70, 182, 282 } };
 	API::effects_bground(lb_pt, effects::bground_transparent(0), 0);
 	lb_pt.typeface(font{
@@ -112,11 +108,57 @@ void the_game() {
 	lb_pt.fgcolor(colors::black);
 	int pt = 0;
 	lb_pt.caption("0");
-	btn_schli_schla.events().click([&lb_pt, &pt] {
+
+	btn_schla.events().click([&lb_pt, &pt] {
 		++pt;
 		lb_pt.caption(std::to_string(pt));
 		});
 	
+	
+	static int i = 2;
+	static int j = 0;
+	lb_cd.caption("2");
+	timer tm{ std::chrono::seconds{1} };
+	tm.elapse([&lb_cd, &tm, &lb_state, &lb_pt, &pt, &btn_schla] {
+		lb_cd.caption(std::to_string(i--));
+		if (i <= -1||j==0) {
+
+			switch (j) {
+			case 0:
+				i = 2;
+				lb_state.caption("Shli ready?");
+				break;
+			case 1:
+				i = 2;
+				lb_state.caption("Shli play!!!");
+				break;
+			case 2:
+				i = 2;
+				lb_state.caption("Shli ended!!!");
+				break;
+			case 3:
+				i = 1;
+				lb_state.caption("Shla ready?");
+				break;
+			case 4:
+				i = 2;
+				lb_state.caption("Shla play!!!");
+				break;
+			case 5:
+				i = 2;
+				lb_state.caption("Shla ended!!!");
+				break;
+			};
+			if (j == 6)
+			{
+				tm.stop();
+				API::exit_all();
+				end();
+			};
+			j++;
+		};
+		});
+	tm.start();
 
 	image img("..\\..\\Schlum\\Schlum\\background2.bmp");
 	drawing dw(fm);
@@ -146,7 +188,7 @@ void entry_window() {
 		});
 	btn.fgcolor(colors::yellow);
 	btn.bgcolor(colors::blue);
-	btn.caption("Schlay!");
+	btn.caption("Play!");
 	btn.events().click([] {
 		API::exit_all();
 		the_game();
@@ -161,22 +203,6 @@ void entry_window() {
 		});
 	lb.fgcolor(colors::yellow);
 	lb.caption("Shli");
-	
-
-	button btn_schli{ fm, rectangle{50, 70, 182, 282} };
-	element::bground bground2;
-	bground2.image(image("..\\..\\Schlum\\Schlum\\schli_bur.bmp"), true, {});
-	API::effects_bground(btn_schli, effects::bground_transparent(0), 0);
-	bground2.stretch_parts(4, 4, 4, 4);
-	btn_schli.set_bground(bground2);
-
-
-	button btn_schla{ fm, rectangle{479, 70, 182, 282} };
-	element::bground bground3;
-	bground3.image(image("..\\..\\Schlum\\Schlum\\schli_bur.bmp"), true, {});
-	API::effects_bground(btn_schla, effects::bground_transparent(0), 0);
-	bground3.stretch_parts(4, 4, 4, 4);
-	btn_schla.set_bground(bground3);
 
 
 	label lb2{ fm, rectangle{ 551, 20, 140, 50 } };
@@ -189,7 +215,7 @@ void entry_window() {
 	lb2.fgcolor(colors::yellow);
 	lb2.caption("Shla");
 
-	image img("..\\..\\Schlum\\Schlum\\background2.bmp");
+	image img("..\\..\\Schlum\\Schlum\\background3.bmp");
 	drawing dw(fm);
 	dw.draw([&img](graphics& graph)
 		{
